3fd0710df135de97ac00b64e9739a2f3780f0a5a
Hung Le <lecaoquochung@gmail.com>
2015-12-11 19:22:33 +0900

    Test PHP check style cakePHP

---
+++ Test/Case/Controller/RanklogsControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+App::uses('RanklogsController', 'Controller');
+
+/**
+ * RanklogsController Test Case
+ */
+class RanklogsControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.ranklog'
+	);
+
+}
